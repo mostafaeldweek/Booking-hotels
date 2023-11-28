0ad3d4bf9c9681fd0cf5fcf29785abdbc38a7a79
mostafa eldweek <mostafaeldweek98@gmail.com>
2023-11-28 18:10:44 +0200

    dddd

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <script setup>
 import Headera from "../components/Header.vue";
+import Headera from "../components/Header.vue";
 import SearchComponents from "../components/SearchComponents.vue";
 import CovidComponent from "../components/CovidComponent.vue";
 import EnjoyComponent from "../components/EnjoyComponent.vue";
